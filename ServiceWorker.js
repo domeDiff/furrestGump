87dd8e98c9f8db3960b37f008afcae9b674deb7b
bitch <169540526+domeDiff@users.noreply.github.com>
2026-09-13 22:41:34 +0530

    build

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "skyLightReflector-furrestGump-1.0";
+const contentToCache = [
+    "Build/web_4.0_no_comp.loader.js",
+    "Build/web_4.0_no_comp.framework.js",
+    "Build/web_4.0_no_comp.data",
+    "Build/web_4.0_no_comp.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
